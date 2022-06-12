520bd9ab17c67d9ab72c19daf623894d9661e838
fikec5431@gmail.com <fikec5431@gmail.com>
2022-06-12 18:42:00 +0200

    Add project files.

---
+++ projekatdame/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekatdame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+        }
+    }
+}
