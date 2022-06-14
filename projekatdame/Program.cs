1b0eafea5e279a79dabd1ed48c5de6a9e19b51d4
Petar Brkić <106657380+Audaciti@users.noreply.github.com>
2022-06-14 16:34:52 +0200

    Ред је на Мику
    урађена главнина програма

--- projekatdame/Program.cs
+++ projekatdame/Program.cs
@@ -8,10 +8,40 @@ namespace projekatdame
 {
     class Program
     {
+        static void Ispisi(int[,] matrica, int a)//mika
+        {
+
+        }
+        static bool Proveri(int[,]matrica, int x, int y, int a)//marko
+        {
+            return true;
+        }
+        static bool NadjiResenje(int[,] tabla, int kolona, int n)
+        {
+            if (kolona >= n)
+            {
+                return true;
+            }
+            for (int pr = 0; pr < n; pr++)
+            {
+                if (Proveri(tabla, pr, kolona, n))//marko
+                {
+                    tabla[pr, kolona] = 1;
+                    if (NadjiResenje(tabla, kolona + 1, n) == true)
+                    {
+                        Ispisi(tabla, n);//mika
+                    }
+                    tabla[pr, kolona] = 0;
+                }
+            }
+            return false;
+        }
         static void Main(string[] args)
         {
-            int N = int.Parse(Console.ReadLine());
-            //test
+            int n = int.Parse(Console.ReadLine());
+            int[,] tabla = new int[n, n];
+            NadjiResenje(tabla, 0, n);
+
         }
     }
 }
