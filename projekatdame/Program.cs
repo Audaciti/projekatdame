b2b3147f0e3b81d809b4cc1b0ca3cd43e02588bc
Petar Brkić <106657380+Audaciti@users.noreply.github.com>
2022-06-16 00:10:16 +0200

    vise izmena
    Dodati komentari, naziv fajla sada sadrzi tacan datum te je moguce cuvati vise fajlova

--- projekatdame/Program.cs
+++ projekatdame/Program.cs
@@ -5,10 +5,12 @@ namespace projekatdame
 {
     class Program
     {
-        static int brojac = 0;
-        static StreamWriter ispis = new StreamWriter("фајл.txt");
-        static void Ispisi(int[,] tabla, int n)
+        static int brojac = 0; //brojac koji broji svaki slucaj
+        static string nazivfajla = "fajl" + DateTime.Now.ToString("yyyyMMddHHmmss")+".txt"; //naziv fajla sadrzi datum kada ce tekstualni fajl biti ispisan
+        static StreamWriter ispis = new StreamWriter(nazivfajla); //definisanje streamwritera koji pravi novi fajl u kome ce biti ispis
+        static void Ispisi(int[,] tabla) //Metod koji ispisuje funkciju
         {
+            int n = tabla.GetLength(0);
             ispis.WriteLine("Slucaj broj " + ++brojac);
             for (int i = 0; i < n; i++)
             {
@@ -18,45 +20,68 @@ namespace projekatdame
             }
             ispis.WriteLine();
         }
-        static bool Bezbedan(int[,] tabla, int red, int kolona, int n)
+        static bool Bezbedan(int[,] tabla, int red, int kolona) /*Funkcija koja gleda da li se dama moze postaviti na odredjeno mesto.
+                                                                 Dovoljno je gledati samo levu stranu, jer dame postavljamo od nulte do n-te kolone*/
         {
             int i, j;
-            for (i = 0; i < kolona; i++)
-                if (tabla[red, i] == 1)
+            int n = tabla.GetLength(0);
+            for (i = 0; i < kolona; i++) //gledamo red sa leve strane
+            {
+                if (tabla[red, i] == 1) //proveravamo da li se negde sa leve strane nalazi dama, ako se nalazi vracamo false
+                {
                     return false;
-            for (i = red, j = kolona; i >= 0 && j >= 0; i--, j--)
+                }
+            }
+            for (i = red, j = kolona; i >= 0 && j >= 0; i--, j--) //proveravamo gornje-levu dijagonalu
+            {
                 if (tabla[i, j] == 1)
+                {
                     return false;
-            for (i = red, j = kolona; j >= 0 && i < n; i++, j--)
+                }
+            }
+            for (i = red, j = kolona; j >= 0 && i < n; i++, j--) //proveravamo donje-levu dijagonalu
                 if (tabla[i, j] == 1)
+                {
                     return false;
-            return true;
+                }
+            return true; //ukoliko se ne nalazi dama ni na jednoj dijagonali niti u istom redu, vracamo true i dama se moze postaviti na zeljeno mesto [i,j]
         }
-        static bool NadjiResenje(int[,] tabla, int kolona, int n)
+        static bool NadjiResenje(int[,] tabla, int kolona)//metoda koja nalazi resenja
         {
-            if (kolona >= n)
+            int n = tabla.GetLength(0);//broj dama
+            if (kolona >= n) //ukoliko smo popunili poslednju kolonu, dosli smo do kraja i ovo je tacno resenje
             {
                 return true;
             }
-            for (int pr = 0; pr < n; pr++)
+            for (int i = 0; i < n; i++) //gledamo ovu kolonu i pokusacemo da postavimo damu na svim mestima jednu po jednu
             {
-                if (Bezbedan(tabla, pr, kolona, n))
+                if (Bezbedan(tabla, i, kolona)) //gledamo da li moze da se postavi
                 {
-                    tabla[pr, kolona] = 1;
-                    if (NadjiResenje(tabla, kolona + 1, n) == true)
+                    tabla[i, kolona] = 1; //stavljamo damu
+                    if (NadjiResenje(tabla, kolona + 1) == true) //ukoliko je resenje tacno, ispisacemo ga
                     {
-                        Ispisi(tabla, n);
+                        Ispisi(tabla);
                     }
-                    tabla[pr, kolona] = 0;
+                    tabla[i, kolona] = 0; //ako ne uspemo da dodjemo do poslednje kolone, znaci da ne dolazimo do resenja; BACKTRACKING, brisemo poslednju postavljenu damu i idemo dalje
                 }
             }
-            return false;
+            return false; //ako se dama ne moze postaviti ni u jednom redu vracamo false
         }
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
+            Console.Write("Unesite broj dama: ");
+            int n = int.Parse(Console.ReadLine()); //broj dama/sirina/duzina table
             int[,] tabla = new int[n, n];
-            NadjiResenje(tabla, 0, n);
+            NadjiResenje(tabla, 0); //pozivamo metodu koja ima zadatak da pronadje sva resenja, uzimamo tablu i pocetnu kolonu
+            if (brojac > 0)
+            {
+                Console.WriteLine("Broj resenja: " + brojac);
+                Console.WriteLine("Kako biste videli sva resenja, pogledajte fajl " + Directory.GetCurrentDirectory() + "\\" + nazivfajla);
+            }
+            else
+            {
+                Console.WriteLine("Nema resenja.");
+            }
             ispis.Close();
         }
     }
